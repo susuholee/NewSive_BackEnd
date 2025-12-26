f08de37b2b5f181c06e9e3b780fd6d054abbe912
blockSuho <akakak695@naver.com>
2025-12-26 19:17:20 +0900

    feat: 유저 조회 API 추가

--- newsive_backend/src/app.module.ts
+++ newsive_backend/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './common/prisma/prisma.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    UsersModule
+  ],
 })
 export class AppModule {}
