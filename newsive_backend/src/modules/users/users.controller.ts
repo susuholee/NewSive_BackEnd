b3a17df147e89f624dcb6a62a3e09ab97576b4a4
blockSuho <akakak695@naver.com>
2025-12-26 21:49:01 +0900

    feat: 유저 회원가입 API 추가

--- newsive_backend/src/modules/users/users.controller.ts
+++ newsive_backend/src/modules/users/users.controller.ts
@@ -1,12 +1,19 @@
-import {Controller, Get } from "@nestjs/common";
-import { UsersService } from "./users.service";
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create_users_dto';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly userService : UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
-    @Get()
-    async getUsers() {
-        return this.userService.findAll();
-    }
+  @Get()
+  getUsers() {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    console.log("여기야 DTO",dto); 
+    return this.usersService.create(dto);
+  }
 }
--- newsive_backend/src/modules/users/users.service.ts
+++ newsive_backend/src/modules/users/users.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/common/prisma/prisma.service";
+import * as bcrypt from "bcrypt";
+import { CreateUserDto } from "./dto/create_users_dto";
+
 
 @Injectable()
 export class UsersService {
@@ -7,5 +10,33 @@ export class UsersService {
 
     async findAll() {
         return this.prisma.user.findMany();
+    }
+
+    async create(dto : CreateUserDto) {
+        const { username, password} = dto
+ 
+
+        // 중복 체크 검증
+        const dupliCate_Check = await this.prisma.user.findUnique({ where : {username}})
+
+        if (dupliCate_Check) {
+            throw new ConflictException("이미 존재하는 아이디 입니다")
+        }
+
+        const hashedPwd = await bcrypt.hash(password, 10);
+
+        const user = await this.prisma.user.create({
+            data : {
+                username, 
+                password : hashedPwd,
+            }
+        })
+
+        return {
+            id : user.id,
+            username : user.username,
+            nickname: user.nickname,
+            createdAt: user.createdAt,
+        }
     }
 }
