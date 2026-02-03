e9a727642a2026d0727ba7b58f8d9e4b47e31750
blockSuho <akakak695@naver.com>
2026-02-03 20:51:29 +0900

    fix:JWT 토큰 발급 수정

--- newsive_backend/src/modules/auth/strategies/jwt.strategy.ts
+++ newsive_backend/src/modules/auth/strategies/jwt.strategy.ts
@@ -1,24 +1,29 @@
-import { Injectable } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { Request } from "express";
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
+    const secret = configService.get<string>('JWT_SECRET');
+    if (!secret) {
+      throw new Error('JWT_SECRET is not defined');
+    }
+
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         ExtractJwt.fromAuthHeaderAsBearerToken(),
-
         (req: Request) => req?.cookies?.accessToken,
       ]),
-      secretOrKey: process.env.JWT_SECRET as string,
+      secretOrKey: secret,
     });
   }
 
   async validate(payload: any) {
     return {
-      id: payload.sub,   
+      id: payload.sub,
       username: payload.username,
     };
   }
