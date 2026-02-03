43c81560dc62744546f05cf04fb60958fabea4a0
blockSuho <akakak695@naver.com>
2026-02-03 19:34:40 +0900

    fix:CORS 헤더 설정 수정

--- newsive_backend/src/main.ts
+++ newsive_backend/src/main.ts
@@ -7,22 +7,30 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: 'https://www.newsive.store',
     credentials: true,
-  })
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+    ],
+  });
+
   app.use(cookieParser());
-  app.use('/images', express.static(join(__dirname, '..',  'public/images')));
-  app.use('/uploads', express.static(join(__dirname, '..',  'public/uploads')));
+  app.use('/images', express.static(join(__dirname, '..', 'public/images')));
+  app.use('/uploads', express.static(join(__dirname, '..', 'public/uploads')));
 
-
-   app.useGlobalPipes(
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
     }),
   );
+
   const port = process.env.PORT || 4000;
   await app.listen(port);
 }
