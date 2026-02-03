e82a96eb1c525e950699c16447a6bfc666507842
blockSuho <akakak695@naver.com>
2026-02-03 20:29:05 +0900

    fix:Auth 모듈 수정

--- newsive_backend/src/modules/auth/auth.module.ts
+++ newsive_backend/src/modules/auth/auth.module.ts
@@ -1,21 +1,25 @@
-import { Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { UsersModule } from "../users/users.module";
-import { AuthController} from "./auth.controller";
-import { AuthService} from "./auth.service";
-import { JwtStrategy } from "./strategies/jwt.strategy";
+import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-    imports : [
-        UsersModule,
-        JwtModule.register({
-            secret : process.env.JWT_SECRET,
-            signOptions : { expiresIn : '1d'}
-        })
-    ],
-    providers : [AuthService, JwtStrategy],
-    controllers : [AuthController],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
-
 export class AuthModule {}
