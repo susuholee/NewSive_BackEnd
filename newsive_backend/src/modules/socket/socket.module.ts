5804aa207b368e945c4bcce3b9a248621f44bdf8
blockSuho <akakak695@naver.com>
2026-02-06 21:35:11 +0900

    fix:소켓 쿠키 인증 수정

--- newsive_backend/src/modules/socket/socket.module.ts
+++ newsive_backend/src/modules/socket/socket.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from '../auth/auth.module';
 import { WsJwtAuthService } from './ws_jwt.service';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-    }),
+    AuthModule,
   ],
   providers: [WsJwtAuthService],
   exports: [WsJwtAuthService],
