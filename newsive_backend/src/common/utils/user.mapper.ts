615b19bd8fd5af1f10643eda24c91597310c52fe
blockSuho <akakak695@naver.com>
2026-02-07 14:34:29 +0900

    fix:이미지 경로 디버깅 수정

--- newsive_backend/src/common/utils/user.mapper.ts
+++ newsive_backend/src/common/utils/user.mapper.ts
@@ -2,7 +2,9 @@ import { DEFAULT_PROFILE_IMAGE_URL } from '../constants/profile.constants';
 
 export function mapUser(user: any) {
   const BASE_URL = process.env.SERVER_URL!;
+  console.log("서버 경로",BASE_URL)
   const path = user.profileImgUrl || DEFAULT_PROFILE_IMAGE_URL;
+  console.log("기본 이미지 경로",path)
 
   if (path.startsWith('http')) {
     return { ...user, profileImgUrl: path };
@@ -12,6 +14,7 @@ export function mapUser(user: any) {
     ? BASE_URL + path
     : BASE_URL + '/' + path;
 
+    console.log("이미지 경로",profileImgUrl)
   return {
     ...user,
     profileImgUrl,
